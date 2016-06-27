b5b79eefbe4e5cb7422a2cca827cdf0cfbc18efa
Maxwell Struever <maxstruever@gmail.com>
2016-06-27 18:12:57 -0400

    big commit loading events

---
+++ index.php
@@ -0,0 +1,89 @@
+<!DOCTYPE html>
+<!-- <html lang="en" ng-app="myapp"> -->
+<html lang="en">
+
+<head>
+	<title>Sisters Brewing</title>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<link rel="stylesheet" href="styles/sistersbrewery.css">
+
+
+</head>
+
+<body data-spy="scroll" data-target="#navscrollspy" data-offset="200">
+	<script>
+		
+	</script>
+	<div class="maincontent" ng-controller="appController as ctrlr">
+		<nav class="navbar navbar-light navbar-fixed-top bg-faded" id="navscrollspy">
+			<a class="navbar-brand" href="#">The Sisters Brewery</a>
+			<ul class="nav navbar-nav pull-xs-right">
+				<li class="nav-item">
+					<a class="nav-link" href="#About">About Us</a>
+				</li>
+				<li class="nav-item">
+					<a class="nav-link" href="#Beers">Our Beers</a>
+				</li>
+				<li class="nav-item">
+					<a class="nav-link" href="#Events">Events</a>
+				</li>
+				<li class="nav-item">
+					<a class="nav-link" href="#wheretofindus">Where To Find Our Beers</a>
+				</li>
+			</ul>
+			<!--<form class="form-inline pull-xs-right">
+    <input class="form-control" type="text" placeholder="Search">
+    <button class="btn btn-success-outline" type="submit">Search</button>
+  </form>-->
+		</nav>
+		<div class="container">
+			<div class="jumbotron">
+				<h1>Sisters Brewing</h1>
+			</div>
+			<h3 id="About">This page </h3>
+			<p>Text</p>
+			<br/>
+			<div class="fb-like" data-href="https://www.facebook.com/thesistersbrewery" data-layout="standard" data-action="like" data-show-faces="false" data-share="false">
+			</div>
+			<br/>
+			<br/>
+			<h1 id="Beers">Our Beers</h1>
+			<p>derp</p>
+			<h1 id="Events">Events</h1>
+			<p>search <input type="text" placeholder="search events" ng-model="eventsearch"></p>
+			<div class="row-fluid" ng-show="events">
+				<div class="event col-md-4" ng-repeat="event in events | filter:eventsearch" ng-hide="Date.Parse(event.start_time) < Date.now()">
+					<h2>{{event.name}}</h2>
+					<h3>{{event.start_time}}</h3>
+					<p>{{event.description}}</p>
+				</div>
+			</div>
+			<h1 id="wheretofindus">Where to Find us</h1>
+			<!--<div class="embed-responsive">-->
+			<div id="findmap">
+				<iframe class="" src="https://www.google.com/maps/d/embed?mid=1I_Got5rCkjb3Eem-CYTImXIFGYc&z=12&ll=52.3665982%2C%204.8851904" width="100%" height="480"></iframe>
+			</div>
+			<!--</div>-->
+		</div>
+	</div>
+	<div class="container-fluid footer">
+		<div class="container">
+			<div class="row">
+				<div class="col-xs-6">Site by Max & Flex</div>
+				<div class="col-xs-6 footer-link"><a class="" href="http://madmaxlax.com">madmaxlax.com</a></div>
+			</div>
+		</div>
+	</div>
+	<script src="//ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
+	<script src="https://ajax.googleapis.com/ajax/libs/angularjs/1.3.14/angular.min.js"></script>
+	<script src="https://ajax.googleapis.com/ajax/libs/angularjs/1.3.14/angular-resource.min.js"></script>
+	<script src="https://ajax.googleapis.com/ajax/libs/angularjs/1.3.14/angular-route.min.js">
+	</script>
+	<script src="./js/app.js">
+	</script>
+	<script src="js/bootstrap.min.js"></script>
+
+</body>
+
+</html>
---
+++ js/app.js
@@ -0,0 +1,113 @@
+window.fbAsyncInit = function() {
+  FB.init({
+    appId: '1007778489291152',
+    xfbml: true,
+    version: 'v2.6'
+  });
+
+  ///only load angular when ready
+  angular.bootstrap(document, ['myapp']);
+};
+
+(function(d, s, id) {
+  var js, fjs = d.getElementsByTagName(s)[0];
+  if (d.getElementById(id)) {
+    return;
+  }
+  js = d.createElement(s);
+  js.id = id;
+  js.src = "//connect.facebook.net/en_US/sdk.js";
+  fjs.parentNode.insertBefore(js, fjs);
+}(document, 'script', 'facebook-jssdk'));
+
+(function() {
+  var app = angular.module('myapp', []);
+
+  //create a factory to do FB calls
+  app.factory('facebookService', function($q) {
+    return {
+      FBCall: function(querystring, options) {
+        var deferred = $q.defer();
+        FB.api(querystring, options, function(response) {
+          if (!response || response.error) {
+            console.log(response);
+            deferred.reject('Error occured');
+          } else {
+            deferred.resolve(response);
+          }
+        });
+        return deferred.promise;
+      }
+    }
+  });
+
+  //set up controller
+  app.controller('appController', function(facebookService, $scope, $window) {
+    //     $window.fbAsyncInit = function() {
+
+    //       //initialize FB
+    //       FB.init({
+    //         appId: '1007778489291152',
+    //         status: true,
+    //         cookie: true,
+    //         xfbml: true,
+    //         version: 'v2.6'
+    //       });
+    //     };
+    //     //
+
+    //     (function(d) {
+    //       // load the Facebook javascript SDK
+
+    //       var js,
+    //         id = 'facebook-jssdk',
+    //         ref = d.getElementsByTagName('script')[0];
+
+    //       if (d.getElementById(id)) {
+    //         return;
+    //       }
+
+    //       js = d.createElement('script');
+    //       js.id = id;
+    //       js.async = true;
+    //       js.src = "//connect.facebook.net/en_US/all.js";
+
+    //       ref.parentNode.insertBefore(js, ref);
+
+    //     }(document));
+
+    //try fb call
+    $scope.events = false;
+    facebookService.FBCall("/thesistersbrewery/events?access_token=1007778489291152|u2Rs03TsG_yGoAxzC8ZUdpgEOwA")
+      .then(function(response) {
+        console.log(response);
+        $scope.events = response.data;
+      });
+
+    //examples
+    this.member = " ";
+    this.method = function() {
+      //do something
+    };
+  });
+})();
+
+// $(document).ready(function() {
+//   $.ajaxSetup({
+//     cache: true
+//   });
+//   $.getScript('//connect.facebook.net/en_US/sdk.js', function() {
+//     FB.init({
+//       appId: '1007778489291152',
+//       xfbml: true,
+//       version: 'v2.6' // or v2.0, v2.1, v2.2, v2.3
+//     });
+//     FB.api(
+//       "/thesistersbrewery/events?access_token=1007778489291152|u2Rs03TsG_yGoAxzC8ZUdpgEOwA",
+//       function(response) {
+//         console.log(response);
+//         if (response && !response.error) {}
+//       }
+//     );
+//   });
+// });
