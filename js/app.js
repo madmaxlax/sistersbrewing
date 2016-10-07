ee3bc4a9f811648e928ce78b2f553dc4f5d48d5d
Max Struever <MaxStruever@gmail.COM>
2016-10-07 12:03:01 +0200

    route fixing and beersdb added

--- js/app.js
+++ js/app.js
@@ -1,4 +1,6 @@
-window.fbAsyncInit = function() {
+/// <reference path="C:\Apps\GitHub\sistersbrewing\js\beersdb.js" />
+
+window.fbAsyncInit = function () {
   FB.init({
     appId: '1007778489291152',
     xfbml: true,
@@ -6,12 +8,12 @@ window.fbAsyncInit = function() {
   });
 
   ///only load angular when ready
-  angular.bootstrap(document, ['myapp']);
+  //angular.bootstrap(document, ['myapp']);
 };
 
 var brewery = false;
 
-(function(d, s, id) {
+(function (d, s, id) {
   var js, fjs = d.getElementsByTagName(s)[0];
   if (d.getElementById(id)) {
     return;
@@ -20,14 +22,14 @@ var brewery = false;
   js.id = id;
   js.src = "https://connect.facebook.net/en_US/sdk.js";//added https for local testing
   fjs.parentNode.insertBefore(js, fjs);
-}(document, 'script', 'facebook-jssdk'));
+} (document, 'script', 'facebook-jssdk'));
 
-(function() {
-  var app = angular.module('SistersBrewApp', ['ngAnimate','ngRoute', 'ngResource']);
-  angular.module('SistersBrewApp').filter('future', function() {
-    return function(items) {
+(function () {
+  var app = angular.module('SistersBrewApp', ['ngAnimate', 'ngRoute', 'ngResource']);
+  angular.module('SistersBrewApp').filter('future', function () {
+    return function (items) {
       var filtered = [];
-      angular.forEach(items, function(event) {
+      angular.forEach(items, function (event) {
         //console.log(event);
         if (event && (event.start_time || event.created_time)) {
           var timest = event.start_time || event.created_time;
@@ -39,150 +41,125 @@ var brewery = false;
       return filtered;
     };
   });
-  angular.module('SistersBrewApp').filter('reverse', function() {
-    return function(items) {
+  angular.module('SistersBrewApp').filter('reverse', function () {
+    return function (items) {
       return items.slice().reverse();
     };
   });
   //create a factory to do FB calls
-  angular.module('SistersBrewApp').factory('facebookService', function($q) {
+  angular.module('SistersBrewApp').factory('facebookService', function ($q) {
     return {
-      FBCall: function(querystring, options) {
+      FBCall: function (querystring, options) {
         var deferred = $q.defer();
-        FB.api(querystring, options, function(response) {
-          if (!response || response.error) {
-            console.log(response);
-            deferred.reject('Error occured');
-          } else {
-            deferred.resolve(response);
-          }
-        });
+        if (typeof (FB) != 'undefined' && FB != null) {
+          FB.api(querystring, options, function (response) {
+            if (!response || response.error) {
+              console.log(response);
+              deferred.reject('Error occured');
+            } else {
+              deferred.resolve(response);
+            }
+          });
+        } else {
+          // alert the user
+        }
+
         return deferred.promise;
       }
     };
   });
 
-  angular.module('SistersBrewApp').config(function($routeProvider){
-        $routeProvider
-        // .when('/beers',{
-        //     templateUrl: 'pages/beer.html',
-        //     controller: 'NotesIndexController',
-        //     controllerAs: 'indexController'
-        // })
-        .when('/beers/:selectedbeer',{
-            templateUrl: 'pages/beer.html',
-            controller: 'BeerController',
-            controllerAs: 'BeerCtrlr'
-        })
-        .when('/about',{
-            templateUrl: 'pages/about.html',
-            controller: 'AboutController',
-            controllerAs: 'AboutCtrlr'
-        })
-        .when('/',{
-            templateUrl: 'pages/index.html',
-            controller: 'IndexController',
-            controllerAs: 'IndexCtrlr'
-          })
-        .otherwise( {redirectTo: '/'});
-    });
+  angular.module('SistersBrewApp').config(function ($routeProvider) {
+    $routeProvider
+      // .when('/beers',{
+      //     templateUrl: 'pages/beer.html',
+      //     controller: 'NotesIndexController',
+      //     controllerAs: 'indexController'
+      // })
+      .when('/beers/:selectedbeer', {
+        templateUrl: 'pages/beer.html',
+        controller: 'BeerController',
+        controllerAs: 'BeerCtrlr'
+      })
+      .when('/about', {
+        templateUrl: 'pages/about.html',
+        controller: 'AboutController',
+        controllerAs: 'AboutCtrlr'
+      })
+      .when('/', {
+        templateUrl: 'pages/index.html',
+        controller: 'IndexController',
+        controllerAs: 'IndexCtrlr'
+      })
+      .otherwise({ redirectTo: '/' });
+  });
 
   //set up main app controller
-  angular.module('SistersBrewApp').controller('appController', function(facebookService, $scope, $window, $http) {
-    //     $window.fbAsyncInit = function() {
-
-    //       //initialize FB
-    //       FB.init({
-    //         appId: '1007778489291152',
-    //         status: true,
-    //         cookie: true,
-    //         xfbml: true,
-    //         version: 'v2.6'
-    //       });
-    //     };
-    //     //
-
-    //     (function(d) {
-    //       // load the Facebook javascript SDK
-
-    //       var js,
-    //         id = 'facebook-jssdk',
-    //         ref = d.getElementsByTagName('script')[0];
-
-    //       if (d.getElementById(id)) {
-    //         return;
-    //       }
-
-    //       js = d.createElement('script');
-    //       js.id = id;
-    //       js.async = true;
-    //       js.src = "//connect.facebook.net/en_US/all.js";
-
-    //       ref.parentNode.insertBefore(js, ref);
-
-    //     }(document));
-
+  angular.module('SistersBrewApp').controller('appController', function (facebookService, $scope, $window, $http) {
 
     //filter for events being older
-    $scope.isFuture = function(event) {
-      return function(item) {
+    $scope.isFuture = function (event) {
+      return function (item) {
         return item[Date.parse(event.start_time)] > Date.now();
-      }
-    }
+      };
+    };
+
+    //make the beersDB available to the main scope
+    $scope.beersDB = beersDB;
 
     $scope.events = false;
     $scope.posts = false;
     $scope.brewery = false;
-    
-    
+
+
     //get untapped info
     $http.defaults.cache = true;
     $http.get('https://api.untappd.com/v4/brewery/info/225097?client_id=43158D6116E0305CADB971CC65769720271E6D6A&client_secret=E1E244AD4D1699C1D3BE949A89EFE7B51E0BE0D5').
-    then(function(data, status, headers, config) {
-      console.log(data);
-      brewery = data.data.response.brewery;
-      $scope.brewery = data.data.response.brewery;
-    }).catch(function(data, status, headers, config) {
-      console.log("Error getting data ",data);
-    });
+      then(function (data, status, headers, config) {
+        console.log(data);
+        brewery = data.data.response.brewery;
+        $scope.brewery = data.data.response.brewery;
+      }).catch(function (data, status, headers, config) {
+        console.log("Error getting data ", data);
+      });
     //try fb call
     //events
     facebookService.FBCall("/thesistersbrewery/events?access_token=1007778489291152|u2Rs03TsG_yGoAxzC8ZUdpgEOwA")
-      .then(function(response) {
+      .then(function (response) {
         console.log(response);
         $scope.events = response.data;
       });
 
     //feed posts (only get 10?)
     facebookService.FBCall("/thesistersbrewery/posts?limit=10&access_token=1007778489291152|u2Rs03TsG_yGoAxzC8ZUdpgEOwA")
-      .then(function(response) {
+      .then(function (response) {
         console.log(response);
         $scope.posts = response.data;
       });
 
     //examples
     this.member = " ";
-    this.method = function() {
+    this.method = function () {
       //do something
     };
   });
-  angular.module('SistersBrewApp').controller('BeerController', function($scope, $routeParams, $http) {
+  angular.module('SistersBrewApp').controller('BeerController', function ($scope, $routeParams, $http) {
     $scope.routeSelectedBeer = $routeParams.selectedbeer;
   });
-  
-  angular.module('SistersBrewApp').controller('AboutController', function($scope, $routeParams, $http) {
-    
+
+  angular.module('SistersBrewApp').controller('AboutController', function ($scope, $routeParams, $http) {
+
   });
-  
-  angular.module('SistersBrewApp').controller('IndexController', function($scope, $routeParams, $http) {
-    
+
+  angular.module('SistersBrewApp').controller('IndexController', function ($scope, $routeParams, $http) {
+
   });
 })();
 
 //easing scrolling
 //no easing /jquery UI needed
-$(function() {
-  $('a[href*=#]:not([href=#])').click(function() {
+$(function () {
+  $('a[href*=#]:not([href=#])').click(function () {
     if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
       var target = $(this.hash);
       var hashstring = this.hash.slice(1);
@@ -193,7 +170,7 @@ $(function() {
 
         $('html,body').stop().animate({
           scrollTop: target.offset().top - 100
-        }, 1000, function() {
+        }, 1000, function () {
           location.hash = hashstring; //attach the hash (#jumptarget) to the pageurl
         });
         return false;
---
+++ js/beersdb.js
@@ -0,0 +1,110 @@
+//Since there is no real backend (and none needed), this is just a 
+//global var where a mongodb-like object is set up.
+
+var beersDB = {
+    queenbee:{
+        name:"Queen Bee",
+        shortDesc: "The Queen Bee is a delicious dry hopped IPA (7.7%) with a smooth citrus hoppy flavor.",
+        longDesc: "Queen Bee is an India Pale Ale.\
+\
+India Pale Ale, abbreviated IPA is a style of beer within the category of Pale Ale. The beer was first brewed in England, destined for the London market in the 19th century. During the 17th century, England had many overseas colonies and the beer for the British soldiers and civilians was sent with ships to the other side of the world. A sailing vessel was on the road to India for nearly five months, far too long to keep normal beer. The English brewers in order to increase the shelf life of the beer, added more hops to the beer and increased the alcohol content of the beer. Hence, all the exported beer was heavily hopped, therefore was more bitter and higher in alcohol content. Thus India Ale, arose during the 18th century . India Pale Ale was created much later.\
+\
+IPA is a very popular craft beer and a beer to enjoy quietly and slowly.",
+        artist: "Kiana Girigorie",
+        artistUrl:"http://instagram.com/kiana.girigorie",
+        backgroundColor: '#e7ece8',
+        labelImageUrl: 'imgs/queenbee.jpg',
+        untappd:'http//',
+    },
+    drone:{
+        name:"Drone",
+        shortDesc: "The Drone, our fantastic Porter (5.8%) has aromas of roasted malt, chocolate, caramel and coffee.",
+        longDesc: 'Drone is a Porter.\
+\
+Porter is a type of dark beer, coming from London, it was first made during the 18th century. It is a derivative of brown beer, made from brown malt with a lot of hop. The beer is called Porter since it was very popular among the street and river porters. In 1802 the writer John Feltham wrote about the history of Porter, which was then used as the basis for most writings on this subject. However, there is very limited contemporary evidence on this version of the history of Porter. Feltham\'s story was based on a letter written by Obadiah Poundage (who for decades worked in the London brewing industry), in the 1760s. Feltham interpreted the text in a different way, most probably not correctly, because he was not familiar with the 18th century brewery terminology. He said that during the 18th century in London there was a popular drink called the "three threads". The three threads was made from a mixture of third of a pint of ale, beer and "twopenny" (the strongest beer, that costs two pence). Around 1730, wrote Feltham, there was a brewer called Harwood who brewed a beer called "Entire" which tasted very similar to the "three threads" which later was known as Porter. Porter is already mentioned in 1721, but only in Feltham\'s writing it states that this beer was made to create the "three threads". Instead, the beer seems to have evolved from the brown beer which was already brewed at that time in London. During 1700 beer brewers sold the beer when it was very young and it matured at the shop. Porter was the first beer that was already matured in the brewery and was ready to drink immediately. It was also the first beer that could be produced on a larger scale. A Porter is a full of flavor beer with lots of dark tones, one will often taste the chocolate flavor within.',
+        artist: "Kiana Girigorie",
+        artistUrl:"http://instagram.com/kiana.girigorie",
+        backgroundColor: '#21afe5',
+        labelImageUrl: 'imgs/drone.jpg',
+        untappd:'http//',
+    },
+    honeyblonde:{
+        name:"Honey Blonde",
+        shortDesc: "ANice blonde beer, the Honey (7.9%) with notes of lemon, orange and star anise",
+        longDesc: "Honey is a Blonde.\
+\
+A Blonde is a beer with a golden yellow colour. Basically is a Blonde beer a beer of high fermentation. The taste of the beer is generally smooth. The light malts have usually not such a distinct flavor, so the flavor comes from the hops and the yeast.",
+        artist: "Luna Bongers",
+        artistUrl:"http://instagram.com/Luna Bongers",
+        backgroundColor: '#8c8a8e',
+        labelImageUrl: 'imgs/honeyblonde.jpg',
+        untappd:'http//',
+    },
+    worker:{
+        name:"Worker",
+        shortDesc: "Our tasty Imperial Saison, the Worker (9.4%), prickly sweet, over-ripe peaches, dates, stone fruit and the rye is very apparent and gives the beer a spicy floral taste.",
+        longDesc: "Worker is an Imperial Saison.\
+\
+Saison is a Belgian type of beer (beer style) of top fermentation with an alcohol content of normally 5 to 6.5%. Saison is the French word for season because this is a seasonal beer. This beer was originally brewed in the winter by the farmers to be drunk by the seasonal workers in the harvest season. The Saison beer had to be brewed in the winter to prevent the beer would be bad during storage. Saison is originally a light, slightly acidic, dry (aftertaste), bitter, spicy/frutie beer, which was brewed during the winter to be drunk during the summer as a thirst quencher. Saisons are now brewed all year round.",
+        artist: "Kiana Girigorie",
+        artistUrl:"http://instagram.com/kiana.girigorie",
+        backgroundColor: '#c0bbdd',
+        labelImageUrl: 'imgs/worker.jpg',
+        untappd:'http//',
+    },
+    smoker:{
+        name:"Smoker",
+        shortDesc: "Smoker (5,4%), a subtle Rauchbier. Very clean smokiness with pleasant sweetness and complex flavours of the malt.",
+        longDesc: "",
+        artist: "Bernadette Leijdekkers",
+        artistUrl:"http://www.leijdekkers.nl/",
+        backgroundColor: '#4897a9',
+        labelImageUrl: 'imgs/worker.jpg',
+        untappd:'http//',
+    },
+    hive:{
+        new:true,
+        name:"Hive",
+        shortDesc: "New beer!",
+        longDesc: "",
+        artist: "Bernadette Leijdekkers",
+        artistUrl:"http://www.leijdekkers.nl/",
+        backgroundColor: '#d34b28',
+        labelImageUrl: 'imgs/worker.jpg',
+        untappd:'http//',
+    },
+    drone2:{
+        name:"Drone²",
+        shortDesc: "Drone² (8,5%), an Imperial version of the Drone. We increased the ingredients to get more aromas of roasted malt, chocolate, caramel and coffee!!!",
+        longDesc: "",
+        artist: "Kiana Girigorie",
+        artistUrl:"http://instagram.com/kiana.girigorie",
+        backgroundColor: '#39a1db',
+        labelImageUrl: 'imgs/drone2.jpg',
+        untappd:'http//',
+    },
+    nectar:{
+        name:"Nectar",
+        shortDesc: "Nectar (10,7%), our Barleywine has a nice balance of malt and bitterness. The hops and malt work very well together.",
+        longDesc: "",
+        artist: "Cristiaan Lieverse",
+        artistUrl:"http://www.christiaanlieverse.com/",
+        backgroundColor: '#c9cacf',
+        labelImageUrl: 'beer.jpg',
+        untappd:'http//',
+    }
+    /*,
+    newbeer:{
+        name:"New Beer",
+        shortDesc: "Short Desc",
+        longDesc: "Long \
+        desc",
+        artist: "Kiana Girigorie",
+        artistUrl:"http://instagram.com/kiana.girigorie",
+        backgroundColor: 'light-green',
+        labelImageUrl: 'beer.jpg',
+        untappd:'http//',
+    },
+    */
+
+};
